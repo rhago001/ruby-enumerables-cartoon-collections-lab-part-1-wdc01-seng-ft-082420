f19f87287830f6d15ef5386a7ae16b62be750e38
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-10 08:40:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |arry, index|
-  print "#{index}: #{arry}/n"
+  print "*#{index}. #{arry}"
 end
 end
